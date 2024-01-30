2b2002a4d2b8a329d2ac9ac190edfe4801c73bc9
BentahriMerwane <m.bentahri@students.ephec.be>
2024-01-30 21:31:21 +0100

    add favicon

--- assets/js/checkboxselect.js
+++ assets/js/checkboxselect.js
@@ -1,4 +1,3 @@
-
 var skillsData = {
   "Languages": [
     "Français",
@@ -35,39 +34,56 @@ var skillsData = {
     "Experience in data backup and recovery"
   ],
   "Certifications": [
-    "Fortinet NSE1/NSE2",
-    "Veeam Backup & Replication"
+    { 
+      name: "Fortinet NSE1/NSE2", 
+      pdfs: [
+        { name: "NSE1 1", link: "assets/Certification/NSE_1_Certification.pdf" },
+        { name: "NSE1 2", link: "assets/Certification/NSE_2_Certification.pdf" }
+      ]
+    },
+    { 
+      name: "Veeam Backup & Replication", 
+      pdfs: [
+        { name: "Veeam Sales", link: "assets/Certification/certificate_SL.pdf" },
+        { name: "Veeam Technicale Sales", link: "assets/Certification/certificate_TSP.pdf" }
+      ]
+    }
   ]
 };
-
 
 var lastChecked = null;
 
 function logSkills(checkbox) {
   var skillName = checkbox.value;
   var skillValues = skillsData[skillName];
-
   var selectedValuesContainer = document.getElementById("item-skills");
-
-
 
   if (lastChecked !== null && lastChecked !== checkbox) {
     lastChecked.checked = false;
   }
   lastChecked = checkbox;
   if (checkbox.checked) {
-
     selectedValuesContainer.innerHTML = "<ul></ul>"
-
     skillValues.forEach(function (value) {
-
-
       var listItem = document.createElement("li");
-      listItem.textContent = "‣ " + value;
+      if (typeof value === "object") {
+        var sublist = document.createElement("ul");
+        value.pdfs.forEach(function(pdf) {
+          var subListItem = document.createElement("li");
+          var link = document.createElement("a");
+          link.href = pdf.link;
+          link.textContent = pdf.name;
+          link.target = "_blank"; // Open link in a new tab
+          
+          subListItem.appendChild(link);
+          sublist.appendChild(subListItem);
+        });
+        listItem.textContent = value.name + ":";
+        listItem.appendChild(sublist);
+      } else {
+        listItem.textContent = "‣ " + value;
+      }
       selectedValuesContainer.appendChild(listItem);
-
     });
-
   }
-
 }
